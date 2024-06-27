f3363a4ab3ddaa4cd5bc4c4a3be5815d77b1bc23
Sukhveer Singh <44468231+veersingh9540@users.noreply.github.com>
2024-06-27 15:33:00 -0400

    Update Finding Nemo(Big O).js

--- Finding Nemo(Big O).js
+++ Finding Nemo(Big O).js
@@ -1,1 +1,32 @@
+const nemo = ["nemo"];
+const everyone = [
+  "dory",
+  "bruce",
+  "marlin",
+  "nemo",
+  "gill",
+  "bloat",
+  "nigel",
+  "squirt",
+  "darla",
+  "hank",
+];
+const large = new Array(1000).fill("nemo");
+// Testing the function O(n) with 3 different Datasets comparing the time taken by each array. 
+function findNemo(array) {
+  let t0 = performance.now();
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === "nemo") {
+      console.log("Found Nemo!");
+    }
+  }
+  let t1 = performance.now();
+  console.log("Call to find Nemo took " + (t1 - t0) + " milliseconds");
+}
+
+// Upon checking We get that the time is increasing linearly that is O(n) --> Linear Time.
+// This is because we have the function looping n time checking each item in the array.
+findNemo(large);
+
+
 
